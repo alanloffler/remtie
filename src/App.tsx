db8c3d4311e55bd6f89fe5ca9822eccdb83f4a57
alanloffler <alanmatiasloffler@gmail.com>
2024-03-18 20:11:52 -0300

    Added new route

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import ProtectedRoutes from './components/login/ProtectedRoutes';
 import Loading2 from '@/components/shared/Loading2';
 // If Layout is lazyloading
 // const Layout = React.lazy(() => import("./components/layout/Layout"));
+const Dashboard = React.lazy(() => import('./components/dashboard/Dashboard'));
 const ListUsers = React.lazy(() => import('./components/users/ListUsers'));
 const CreateNewUser = React.lazy(() => import('./components/users/CreateNewUser'));
 const UpdateUser = React.lazy(() => import('./components/users/UpdateUser'));
@@ -25,7 +26,7 @@ const appUrl: string = import.meta.env.VITE_APP_URL;
 // React component
 function App() {
 	const routes = [
-		{ path: `${appUrl}/`, element: <ListProducts /> },
+		{ path: `${appUrl}/`, element: <Dashboard /> },
 		{ path: `${appUrl}/usuarios`, element: <ListUsers /> },
 		{ path: `${appUrl}/usuario/:id`, element: <ViewUser /> },
 		{ path: `${appUrl}/usuario/crear`, element: <CreateNewUser /> },
@@ -50,6 +51,7 @@ function App() {
 						}>
 						{routes.map((route) => (
 							<Route 
+                                key={route.path}
                                 path={route.path} 
                                 element={
                                     <React.Suspense fallback={<Loading2 width='60' height='60' color='#0ea5e9' dur={0.75} />}>

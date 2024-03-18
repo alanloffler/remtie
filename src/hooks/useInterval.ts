adc5d01961ea3699d23c87e260ed2e3dec3ee422
alanloffler <alanmatiasloffler@gmail.com>
2024-03-18 20:11:17 -0300

    Custom hook

---
+++ src/hooks/useInterval.ts
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from 'react';
+
+function useInterval(callback: () => void, delay: number | null): void {
+	const savedCallback = useRef(() => {});
+
+	useEffect(() => {
+		savedCallback.current = callback;
+	}, [callback]);
+
+	useEffect(() => {
+		function tick() {
+			savedCallback.current();
+		}
+		if (delay !== null) {
+			const id = setInterval(tick, delay);
+			return () => clearInterval(id);
+		}
+	}, [delay]);
+}
+export default useInterval;

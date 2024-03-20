b25e179c18bc0aaff3868212abcfad1fc4ec544a
alanloffler <alanmatiasloffler@gmail.com>
2024-03-20 18:53:01 -0300

    Date custom hook creation

---
+++ src/hooks/useLocaleDate.ts
@@ -0,0 +1,16 @@
+function useLocaleDate() {
+	return (date: string | undefined) => {
+		if (date) {
+			const newDate = new Date(date);
+			console.log(newDate);
+			const day: string = String(newDate.getDate()).padStart(2, '0');
+			const month: string = String(newDate.getMonth() + 1).padStart(2, '0');
+			const year: string = String(newDate.getFullYear()).slice(-2);
+			return `${day}/${month}/${year}`;
+		} else {
+			return 'No date provided';
+		}
+	};
+}
+
+export { useLocaleDate };

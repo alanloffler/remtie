a7194b1db235376af7a828f1302c767a99751b9e
alanloffler <alanmatiasloffler@gmail.com>
2024-03-20 12:10:50 -0300

    Dashboard services creation

---
+++ src/services/dashboard.services.ts
@@ -0,0 +1,25 @@
+export class DashboardServices {
+	static readonly apiUrl: string = import.meta.env.VITE_REACT_BACKEND_API;
+
+	static async getPropertiesByCategory() {
+		try {
+			const request: Response = await fetch(`${DashboardServices.apiUrl}/dashboard/properties/categories`, {
+				method: 'GET'
+			});
+			return await request.json();
+		} catch (error) {
+			return error;
+		}
+	}
+
+    static async getLatestProperties(id: number) {
+        try {
+            const request: Response = await fetch(`${DashboardServices.apiUrl}/dashboard/properties/latest/${id}`, {
+                method: 'GET'
+            });
+            return await request.json();
+        } catch (error) {
+            return error;
+        }
+    }
+}

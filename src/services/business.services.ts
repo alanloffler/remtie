1ab677d4a29edaa6f6e528c49a3e13ce16ec69a0
alanloffler <alanmatiasloffler@gmail.com>
2024-04-16 17:19:51 -0300

    Rename service

--- src/services/business.services.ts
+++ src/services/business.services.ts
@@ -1,22 +1,21 @@
 import { store } from "./store.services";
 
-// Business service: used in the business type select input
 export class BusinessServices {
-	static readonly apiUrl: string = import.meta.env.VITE_REACT_BACKEND_API;
+	static readonly API_URL: string = import.meta.env.VITE_REACT_BACKEND_API;
 
-	static async getBusiness() {
+	static async findAll() {
 		try {
             const token = store.getState().authToken;
-			const query: Response = await fetch(`${BusinessServices.apiUrl}/business`, {
+			const query: Response = await fetch(`${BusinessServices.API_URL}/business`, {
 				method: 'GET',
 				headers: {
                     'content-type': 'application/json;charset=UTF-8',
-                    Authorization: 'Bearer ' + token
+                    Authorization: `Bearer ${token}`
                 }
 			});
             return await query.json();
-		} catch (e) {
-			return e;
+		} catch (error) {
+			return error;
 		}
 	}
 }
--- src/services/categories.services.ts
+++ src/services/categories.services.ts
@@ -1,22 +1,21 @@
-import { store } from "./store.services";
+import { store } from './store.services';
 
-// Categories service: used in the categories select input
 export class CategoriesServices {
 	static readonly API_URL: string = import.meta.env.VITE_REACT_BACKEND_API;
 
-	static async getCategories() {
+	static async findAll() {
 		try {
-            const token = store.getState().authToken;
+			const token = store.getState().authToken;
 			const query: Response = await fetch(`${CategoriesServices.API_URL}/categories`, {
 				method: 'GET',
 				headers: {
-                    'content-type': 'application/json;charset=UTF-8',
-                    Authorization: 'Bearer ' + token
-                }
+					'content-type': 'application/json;charset=UTF-8',
+					Authorization: `Bearer ${token}`
+				}
 			});
-            return await query.json();
-		} catch (e) {
-			return e;
+			return await query.json();
+		} catch (error) {
+			return error;
 		}
 	}
 }

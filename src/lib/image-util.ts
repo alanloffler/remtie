0bc4eda5c87f68421a744fcde598a0d377fc150f
alanloffler <alanmatiasloffler@gmail.com>
2024-04-16 17:01:58 -0300

    Changed photos path

--- src/lib/image-util.ts
+++ src/lib/image-util.ts
@@ -1,8 +1,9 @@
 /* Used for vite.js only
-* Function for images path generated dynamically (like in Carousel)
-*/
+ * Function for images path generated dynamically (like in Carousel)
+ */
 function getImageURL(name: string) {
-	return new URL(`../../../api-nest-mysql/uploads/${name}`, import.meta.url).href;
+	// return new URL(`../../../api-nest-mysql/uploads/${name}`, import.meta.url).href;
+	return new URL(`../assets/photos/${name}`, import.meta.url).href;
 }
 
 export { getImageURL };

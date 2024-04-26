ebad50a35bc9eb5de619313eb2bb17d9a563952b
alanloffler <alanmatiasloffler@gmail.com>
2024-04-26 17:58:26 -0300

    Refactoring functions if Admin is logged in

--- src/components/products/UpdateProduct.tsx
+++ src/components/products/UpdateProduct.tsx
@@ -1,5 +1,5 @@
 // Icons: Lucide (https://lucide.dev/)
-import { ArrowLeft, Trash2, BadgeX } from 'lucide-react';
+import { ArrowLeft, Trash2, BadgeX, CheckCircle } from 'lucide-react';
 // UI: Shadcn-ui (https://ui.shadcn.com/)
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
@@ -41,11 +41,12 @@ function UpdateProduct() {
 	const [businessKey, setBusinessKey] = useState<number>(0);
 	const [categories, setCategories] = useState<ICategory[]>([]);
 	const [categoriesKey, setCategoriesKey] = useState<number>(0);
-	const [deleteAction, setDeleteAction] = useState<string>('');
-	const [imageDialog, setImageDialog] = useState<IImage>({ id: 0, name: '', propertyId: 0 });
+	const [action, setAction] = useState<string>('');
+	const [imageDialog, setImageDialog] = useState<IImage>({ id: 0, name: '', propertyId: 0, deletedAt: '' });
 	const [images, setImages] = useState<IImage[]>([]);
 	const [openDialog, setOpenDialog] = useState<boolean>(false);
 	const [property, setProperty] = useState<IProperty>({} as IProperty);
+    const [updateUI, setUpdateUI] = useState<number>(0);
 
 	const propertyForm = useForm<z.infer<typeof propertySchema>>({
 		resolver: zodResolver(propertySchema),
@@ -70,7 +71,7 @@ function UpdateProduct() {
 			file: undefined
 		}
 	});
-    // #region Find property
+	// #region Find property
 	useEffect(() => {
 		ProductsServices.findOne(propertyId).then((response) => {
 			if (response.id > 0) {
@@ -87,12 +88,8 @@ function UpdateProduct() {
 				propertyForm.setValue('price', response.price);
 				propertyForm.setValue('is_active', Boolean(response.is_active));
 			}
-            if (response.statusCode > 399) toast({ title: response.statusCode, description: response.message, variant: 'destructive', duration: 5000 });
-            if (response instanceof Error) toast({ title: 'Error', description: '500 Internal Server Error | ' + response.message, variant: 'destructive', duration: 5000 });
-		});
-
-		ImageServices.findByProperty(propertyId).then((response) => {
-			setImages(response);
+			if (response.statusCode > 399) toast({ title: response.statusCode, description: response.message, variant: 'destructive', duration: 5000 });
+			if (response instanceof Error) toast({ title: 'Error', description: '500 Internal Server Error | ' + response.message, variant: 'destructive', duration: 5000 });
 		});
 
 		BusinessServices.findAll().then((response) => {
@@ -105,8 +102,14 @@ function UpdateProduct() {
 			setCategoriesKey(Math.random());
 		});
 	}, [propertyId, propertyForm]);
-    // #endregion
-    // #region Submit actions
+
+    useEffect(() => {
+		ImageServices.findByProperty(propertyId).then((response) => {
+			setImages(response);
+		});
+    }, [propertyId, updateUI]);
+	// #endregion
+	// #region Submit actions
 	function handleSubmitProduct(values: z.infer<typeof propertySchema>) {
 		const color: string | undefined = categories.find((cat) => cat.name === values.type)?.color;
 		const isActive: number = values.is_active === true ? 1 : 0;
@@ -147,22 +150,30 @@ function UpdateProduct() {
 			.catch((error) => console.log(error)); // toast here
 	}
 
-	function handleDeleteImage(id: number) {
-		if (deleteAction === 'softDelete') {
+	function handleDeleteImage(id: number, action: string) {
+		if (action === 'removeSoft') {
 			ImageServices.removeSoft(id).then((response) => {
-				if (response.status === 200) {
+				if (response.statusCode === 200) {
 					setImages(images.filter((img) => img.id !== id));
+                    setUpdateUI(Math.random());
 				}
 			});
 		}
-		if (deleteAction === 'hardDelete') {
+        if (action === 'restore') {
+            console.log('restore');
+            // ImageServices.restore(id).then((response) => {
+            //     if (response.status === 200) {
+            //         setImages(images.filter((img) => img.id !== id));
+            //     }
+            // });
+        }
+		if (action === 'remove') {
 			ImageServices.remove(id).then((response) => {
-				if (response.status === 200) {
-					setImages(images.filter((img) => img.id !== id));
+				if (response.statusCode === 200) {
+					setUpdateUI(Math.random());
 				}
 			});
 		}
-
 		setOpenDialog(false);
 	}
 
@@ -427,23 +438,37 @@ function UpdateProduct() {
 										</div>
 										<div className='hidden flex-row text-xs font-light text-slate-400 xs:block md:block lg:block'>{img.name}</div>
 										<div className='flex flex-row gap-2'>
-											<Button
-												onClick={() => {
-													setOpenDialog(true);
-													setImageDialog({ id: img.id, name: img.name, propertyId: img.propertyId });
-													setDeleteAction('softDelete');
-												}}
-												variant='ghost'
-												size='miniIcon'
-												className='rounded-full border bg-white text-slate-400/70 shadow-sm hover:bg-white hover:text-rose-500'>
-												<Trash2 className='h-4 w-4' />
-											</Button>
+											{img.deletedAt === null ? (
+												<Button
+													onClick={() => {
+														setOpenDialog(true);
+														setImageDialog({ id: img.id, name: img.name, propertyId: img.propertyId, deletedAt: img.deletedAt });
+														setAction('removeSoft');
+													}}
+													variant='ghost'
+													size='miniIcon'
+													className='rounded-full border bg-white text-slate-400/70 shadow-sm hover:bg-white hover:text-rose-500'>
+													<Trash2 className='h-4 w-4' />
+												</Button>
+											) : (
+												<Button
+													onClick={() => {
+														setOpenDialog(true);
+														setImageDialog({ id: img.id, name: img.name, propertyId: img.propertyId, deletedAt: img.deletedAt });
+														setAction('restore');
+													}}
+													variant='ghost'
+													size='miniIcon'
+													className='rounded-full border bg-white text-slate-400/70 shadow-sm hover:bg-white hover:text-emerald-400'>
+													<CheckCircle className='h-4 w-4' />
+												</Button>
+											)}
 											{store.getState().role === Roles.ADMIN && (
 												<Button
 													onClick={() => {
 														setOpenDialog(true);
-														setImageDialog({ id: img.id, name: img.name, propertyId: img.propertyId });
-														setDeleteAction('hardDelete');
+														setImageDialog({ id: img.id, name: img.name, propertyId: img.propertyId, deletedAt: img.deletedAt });
+														setAction('remove');
 													}}
 													variant='ghost'
 													size='miniIcon'
@@ -475,7 +500,7 @@ function UpdateProduct() {
 											<Button variant='ghost' onClick={() => setOpenDialog(false)}>
 												Cancelar
 											</Button>
-											<Button variant='delete' onClick={() => handleDeleteImage(imageDialog.id)}>
+											<Button variant='delete' onClick={() => handleDeleteImage(imageDialog.id, action)}>
 												Eliminar
 											</Button>
 										</div>
@@ -484,38 +509,40 @@ function UpdateProduct() {
 							</DialogContent>
 						</Dialog>
 					</div>
-					<FormProvider {...imageForm}>
-						<form onSubmit={imageForm.handleSubmit(handleSubmitImage)}>
-							<div className='mt-8 grid w-full max-w-sm items-center gap-1.5'>
-								<FormField
-									control={imageForm.control}
-									name='file'
-									render={() => (
-										<FormItem className='w-full space-y-1'>
-											<FormLabel className='font-semibold text-slate-500'>Agregar imágen</FormLabel>
-											<FormControl>
-												<Input {...imageForm.register('file')} name='file' type='file' accept='image/*' className='w-full hover:cursor-pointer' />
-											</FormControl>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
-							</div>
-							<div className='mt-8 flex flex-row justify-end space-x-4'>
-								<Button
-									variant='ghost'
-									onClick={(e) => {
-										e.preventDefault();
-										imageForm.reset();
-									}}>
-									Cancelar
-								</Button>
-								<Button variant='default' size='default' className='w-auto' type='submit'>
-									Guardar
-								</Button>
-							</div>
-						</form>
-					</FormProvider>
+					{store.getState().userId === property.created_by && (
+						<FormProvider {...imageForm}>
+							<form onSubmit={imageForm.handleSubmit(handleSubmitImage)}>
+								<div className='mt-8 grid w-full max-w-sm items-center gap-1.5'>
+									<FormField
+										control={imageForm.control}
+										name='file'
+										render={() => (
+											<FormItem className='w-full space-y-1'>
+												<FormLabel className='font-semibold text-slate-500'>Agregar imágen</FormLabel>
+												<FormControl>
+													<Input {...imageForm.register('file')} name='file' type='file' accept='image/*' className='w-full hover:cursor-pointer' />
+												</FormControl>
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+								</div>
+								<div className='mt-8 flex flex-row justify-end space-x-4'>
+									<Button
+										variant='ghost'
+										onClick={(e) => {
+											e.preventDefault();
+											imageForm.reset();
+										}}>
+										Cancelar
+									</Button>
+									<Button variant='default' size='default' className='w-auto' type='submit'>
+										Guardar
+									</Button>
+								</div>
+							</form>
+						</FormProvider>
+					)}
 				</div>
 			</div>
 		</main>

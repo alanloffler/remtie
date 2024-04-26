d3cc23e86d819b881dcc5a2c5db9aab51e672ba9
alanloffler <alanmatiasloffler@gmail.com>
2024-04-26 17:57:14 -0300

    Added dropdown menu for user management

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,5 +1,5 @@
 // Icons: Lucide (https://lucide.dev/)
-import { Home, LayoutDashboard, LogOut, Settings, UserRound } from 'lucide-react';
+import { Ellipsis, Home, LayoutDashboard, LogOut, Settings, UserRound } from 'lucide-react';
 // UI: Shadcn-ui (https://ui.shadcn.com/)
 import { Button } from '@/components/ui/button';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
@@ -74,18 +74,14 @@ function Sidebar() {
                 md:translate-x-0`}>
 			<div className='group flex w-full flex-col gap-4 py-2'>
 				<div className='fixed left-0 top-0 h-12 w-full bg-slate-100'></div>
-				<div className='fixed left-0 top-2 flex w-full flex-row justify-between px-3'>
+				<div className='fixed left-0 top-0 flex h-12 w-full flex-row items-center justify-between px-3'>
 					<UserLogged user={userId} />
 					<DropdownMenu>
-						<DropdownMenuTrigger className='rounded-md bg-transparent outline-none hover:bg-slate-300'>
-							<Button variant='ghost' size='miniIcon'>
-								<svg width='18' height='18' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' className='lucide lucide-ellipsis-vertical'>
-									<circle cx='12' cy='12' r='1' />
-									<circle cx='12' cy='5' r='1' />
-									<circle cx='12' cy='19' r='1' />
-								</svg>
-							</Button>
-						</DropdownMenuTrigger>
+						<Button variant='ghost' size='miniIcon' className='hover:bg-slate-200 focus-visible:ring-transparent focus-visible:ring-offset-0' asChild>
+							<DropdownMenuTrigger>
+                                <Ellipsis strokeWidth='2' className='h-4 w-4 text-slate-500' />
+							</DropdownMenuTrigger>
+						</Button>
 						<DropdownMenuContent align='end'>
 							<DropdownMenuItem className='gap-2' onClick={() => navigate(`${APP_URL}/usuario/${userId}`)}>
 								<UserRound strokeWidth='2' className='h-4 w-4' />

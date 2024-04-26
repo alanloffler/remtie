a4c3e1ca9b9b5977ef460efb9f9abb37b0ef2315
alanloffler <alanmatiasloffler@gmail.com>
2024-04-26 17:59:08 -0300

    Code organization and vars typed

--- src/services/image.services.ts
+++ src/services/image.services.ts
@@ -1,5 +1,5 @@
 import { FieldValues } from 'react-hook-form';
-import { IImage } from '@/lib/interfaces/image.interface';
+// import { IImage } from '@/lib/interfaces/image.interface';
 import { store } from '@/services/store.services';
 import { Roles } from '@/lib/constants';
 
@@ -8,13 +8,9 @@ export class ImageServices {
 
 	static async findByProperty(id: number) {
 		try {
-			const token = store.getState().authToken;
-            let sql: string = '';
-            if (store.getState().role === Roles.ADMIN) {
-                sql = `${ImageServices.API_URL}/images/${id}/allByPropertyWithDeleted`;
-            } else {
-                sql = `${ImageServices.API_URL}/images/${id}/allByProperty`;
-            }
+			const token: string = store.getState().authToken;
+			let sql: string = `${ImageServices.API_URL}/images/${id}/allByProperty`;
+			if (store.getState().role === Roles.ADMIN) sql = `${ImageServices.API_URL}/images/${id}/allByPropertyWithDeleted`;
 			const query: Response = await fetch(sql, {
 				method: 'GET',
 				headers: {
@@ -24,16 +20,17 @@ export class ImageServices {
 			});
 			return await query.json();
 		} catch (error) {
-            return error;
+			return error;
 		}
 	}
 
 	static async create(id: number, data: FieldValues) {
-		const formData = new FormData();
+		const formData: FormData = new FormData();
 		formData.append('file', data as Blob);
 		try {
 			const token: string = store.getState().authToken;
-			const query: Response = await fetch(`${ImageServices.API_URL}/images/upload/${id}/`, {
+			const sql: string = `${ImageServices.API_URL}/images/upload/${id}/`;
+			const query: Response = await fetch(sql, {
 				method: 'POST',
 				body: formData,
 				// No other headers because of boundary in multipart/form-data
@@ -44,16 +41,17 @@ export class ImageServices {
 			return error;
 		}
 	}
-    // TODO RENAME FUNCTIONS
+
 	static async removeSoft(id: number) {
 		try {
-            const token: string = store.getState().authToken;
-			const query: Response = await fetch(`${ImageServices.API_URL}/images/${id}/soft`, {
+			const token: string = store.getState().authToken;
+			const sql: string = `${ImageServices.API_URL}/images/${id}/soft`;
+			const query: Response = await fetch(sql, {
 				method: 'DELETE',
 				headers: {
-                    'content-type': 'application/json;charset=UTF-8',
-                    Authorization: `Bearer ${token}`
-                }
+					'content-type': 'application/json;charset=UTF-8',
+					Authorization: `Bearer ${token}`
+				}
 			});
 			return await query.json();
 		} catch (error) {
@@ -62,30 +60,16 @@ export class ImageServices {
 	}
 	static async remove(id: number) {
 		try {
-            const token: string = store.getState().authToken;
-			const query: Response = await fetch(`${ImageServices.API_URL}/images/${id}`, {
+			const token: string = store.getState().authToken;
+			const sql: string = `${ImageServices.API_URL}/images/${id}`;
+			const query: Response = await fetch(sql, {
 				method: 'DELETE',
 				headers: {
-                    'content-type': 'application/json;charset=UTF-8',
-                    Authorization: `Bearer ${token}`
-                }
-			});
-			return await query.json();
-		} catch (error) {
-			return error;
-		}
-	}
-
-	static async removeMany(ids: IImage[]) {
-		try {
-			return await Promise.all(ids.map((image) => ImageServices.remove(image.id))).then((response) => {
-				const allOk = response.some((response) => response.status === 400);
-				if (allOk) {
-					return { status: 400, message: '400 Bad Request | Images not deleted' };
-				} else {
-					return { status: 200, message: '200 OK | Images deleted' };
+					'content-type': 'application/json;charset=UTF-8',
+					Authorization: `Bearer ${token}`
 				}
 			});
+			return await query.json();
 		} catch (error) {
 			return error;
 		}

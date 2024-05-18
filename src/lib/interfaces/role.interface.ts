45f357eda14145e4cbd54ac1c0f02979cce3c99c
alanloffler <alanmatiasloffler@gmail.com>
2024-05-18 13:55:54 -0300

    Role interface created

---
+++ src/lib/interfaces/role.interface.ts
@@ -0,0 +1,12 @@
+export interface IRole {
+    id: number;
+    name: string;
+    title: string;
+    value: string;
+}
+
+export interface IRoleForm {
+    name: string;
+    title: string;
+    value: string;
+}

93d0775ab45298a27434c3c4ebee40f369def07b
alanloffler <alanmatiasloffler@gmail.com>
2024-05-18 13:55:23 -0300

    Config file created

---
+++ src/lib/config/products.config.ts
@@ -0,0 +1,25 @@
+export const ProductsConfig = {
+	contentStatus: {
+		admin: 'Aún no hay propiedades creadas por los usuarios o por vos.',
+		user: 'No hay propiedades creadas por vos'
+	},
+    dialog: {
+        impossibleRevertion: 'Esta acción es posible de revertir por el administrador',
+        possibleRevertion: 'Esta acción es posible de revertir por el administrador',
+        propertyDelete: 'La propiedad va a ser eliminada permanentemente de la base de datos.',
+        propertySoftDelete: 'La propiedad va a ser eliminada de la base de datos.',
+        propertyRestore: 'La propiedad va a ser restaurada de la base de datos.',
+        title: '¿Estás realmente seguro?',
+    },
+    filters: {
+        byCreator: 'Filtrar por creadores'
+    },
+    headers: ['#', 'Tipo', 'Categoría', 'Título', 'Ciudad', 'Precio', 'Acciones'],
+	noResults: 'Sin resultados',
+	pagination: {
+		page: 'Página',
+		of: 'de',
+		rowsPerPage: 'Filas por página'
+	},
+    search: 'Buscar por ciudad'
+};
---
+++ src/lib/config/settings.config.ts
@@ -0,0 +1,37 @@
+export const SettingsConfig = {
+	title: 'Configuraciones generales',
+	sections: {
+		businessType: 'Tipo de negocio',
+		categoriesType: 'Tipo de categoría',
+		defaultView: {
+			title: 'Vista predeterminada',
+			subtitle: 'La vista actual es'
+		},
+        rowsPerPage: {
+            title: 'Opciones de paginación',
+            subtitle1: 'Elementos por página: ',
+            subtitle2: 'Menú de opciones'
+        },
+        roles: {
+            title: 'Roles de usuarios'
+        }
+	},
+	common: {
+		color: 'Color (#000000)',
+		name: 'Nombre',
+        no_data: 'Sin datos',
+		plural: 'Plural',
+		property: 'Propiedades',
+        select: 'Seleccionar',
+        title: 'Título',
+        value: 'Valor',
+		views: {
+			card: 'Tarjeta',
+			list: 'Lista'
+		}
+	},
+	buttons: {
+		back: 'Volver',
+		save: 'Guardar'
+	}
+};

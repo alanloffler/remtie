a211b8a5fcefbd9e1ec564a955df9f2d9178e336
alanloffler <alanmatiasloffler@gmail.com>
2024-05-18 13:48:24 -0300

    Buttons config file created

---
+++ src/lib/config/buttons.config.ts
@@ -0,0 +1,19 @@
+export const ButtonsConfig = {
+    actions: {
+        back: 'Volver',
+        cancel: 'Cancelar',
+        create: 'Nuevo',
+        edit: 'Editar',
+        delete: 'Eliminar',
+        restore: 'Restaurar',
+        save: 'Guardar'
+    },
+    filters: {
+        all: 'Todas',
+        yours: 'Tuyas',
+        others: 'Otros',
+        favorites: 'Tus favoritos',
+    },
+    category: 'Categoría',
+    type: 'Tipo',
+}

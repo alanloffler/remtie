de49ac3abbf20deda38e220c005cc558d7a5eb98
alanloffler <alanmatiasloffler@gmail.com>
2024-05-18 13:43:55 -0300

    Login config file created

---
+++ src/lib/config/login.config.ts
@@ -0,0 +1,11 @@
+export const LoginConfig = {
+	title: 'Remtie',
+	subtitle: 'Ingresá tus datos para acceder a la aplicación',
+	email: 'E-mail',
+    password: 'Contraseña',
+    login: 'Ingresar',
+    errors: {
+        invalidEmail: 'Formato de e-mail inválido',
+        invalidPassword: 'La contraseña debe poseer al menos 6 caracteres',
+    }
+};

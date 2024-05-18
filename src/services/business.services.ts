b92b3047d63527920e2ebac64bf05ca0f7bd0150
alanloffler <alanmatiasloffler@gmail.com>
2024-05-18 13:50:08 -0300

    Added functions to service

--- src/services/business.services.ts
+++ src/services/business.services.ts
@@ -1,19 +1,101 @@
-import { store } from "./store.services";
+import { IBusinessForm } from '@/lib/interfaces/inputs.interface';
+import { store } from './store.services';
 
 export class BusinessServices {
 	static readonly API_URL: string = import.meta.env.VITE_REACT_BACKEND_API;
 
-	static async findAll() {
+	static async findAllUI() {
 		try {
-            const token = store.getState().authToken;
+			const token = store.getState().authToken;
 			const query: Response = await fetch(`${BusinessServices.API_URL}/business`, {
 				method: 'GET',
 				headers: {
+					'content-type': 'application/json;charset=UTF-8',
+					Authorization: `Bearer ${token}`
+				}
+			});
+			return await query.json();
+		} catch (error) {
+			return error;
+		}
+	}
+
+    static async findAll() {
+		try {
+			const token = store.getState().authToken;
+			const query: Response = await fetch(`${BusinessServices.API_URL}/business/withDeleted`, {
+				method: 'GET',
+				headers: {
+					'content-type': 'application/json;charset=UTF-8',
+					Authorization: `Bearer ${token}`
+				}
+			});
+			return await query.json();
+		} catch (error) {
+			return error;
+		}
+	}
+
+    static async create(data: IBusinessForm) {
+		try {
+			const token = store.getState().authToken;
+			const query: Response = await fetch(`${BusinessServices.API_URL}/business`, {
+				method: 'POST',
+                body: JSON.stringify(data),
+				headers: {
+					'content-type': 'application/json;charset=UTF-8',
+					Authorization: `Bearer ${token}`
+				}
+			});
+			return await query.json();
+		} catch (error) {
+			return error;
+		}
+	}
+
+    static async restore(id: number) {
+        try {
+            const token = store.getState().authToken;
+            const query: Response = await fetch(`${BusinessServices.API_URL}/business/${id}/restore`, {
+                method: 'PATCH',
+                headers: {
                     'content-type': 'application/json;charset=UTF-8',
                     Authorization: `Bearer ${token}`
                 }
-			});
+            });
             return await query.json();
+        } catch (error) {
+            return error;
+        }
+    }
+
+	static async removeSoft(id: number) {
+		try {
+			const token = store.getState().authToken;
+			const query: Response = await fetch(`${BusinessServices.API_URL}/business/${id}/soft`, {
+				method: 'DELETE',
+				headers: {
+					'content-type': 'application/json;charset=UTF-8',
+					Authorization: `Bearer ${token}`
+				}
+			});
+			return await query.json();
+		} catch (error) {
+			return error;
+		}
+	}
+
+    static async remove(id: number) {
+		try {
+			const token = store.getState().authToken;
+			const query: Response = await fetch(`${BusinessServices.API_URL}/business/${id}`, {
+				method: 'DELETE',
+				headers: {
+					'content-type': 'application/json;charset=UTF-8',
+					Authorization: `Bearer ${token}`
+				}
+			});
+			return await query.json();
 		} catch (error) {
 			return error;
 		}
--- src/services/categories.services.ts
+++ src/services/categories.services.ts
@@ -1,13 +1,95 @@
+import { ICategoryForm } from '@/lib/interfaces/inputs.interface';
 import { store } from './store.services';
 
 export class CategoriesServices {
 	static readonly API_URL: string = import.meta.env.VITE_REACT_BACKEND_API;
 
-	static async findAll() {
+	static async findAllUI() {
 		try {
 			const token = store.getState().authToken;
 			const query: Response = await fetch(`${CategoriesServices.API_URL}/categories`, {
 				method: 'GET',
+				headers: {
+					'content-type': 'application/json;charset=UTF-8',
+					Authorization: `Bearer ${token}`
+				}
+			});
+			return await query.json();
+		} catch (error) {
+			return error;
+		}
+	}
+	
+    static async findAll() {
+		try {
+			const token = store.getState().authToken;
+			const query: Response = await fetch(`${CategoriesServices.API_URL}/categories/withDeleted`, {
+				method: 'GET',
+				headers: {
+					'content-type': 'application/json;charset=UTF-8',
+					Authorization: `Bearer ${token}`
+				}
+			});
+			return await query.json();
+		} catch (error) {
+			return error;
+		}
+	}
+
+    static async create(data: ICategoryForm) {
+		try {
+			const token = store.getState().authToken;
+			const query: Response = await fetch(`${CategoriesServices.API_URL}/categories`, {
+				method: 'POST',
+                body: JSON.stringify(data),
+				headers: {
+					'content-type': 'application/json;charset=UTF-8',
+					Authorization: `Bearer ${token}`
+				}
+			});
+			return await query.json();
+		} catch (error) {
+			return error;
+		}
+	}
+
+    static async restore(id: number) {
+        try {
+            const token = store.getState().authToken;
+            const query: Response = await fetch(`${CategoriesServices.API_URL}/categories/${id}/restore`, {
+                method: 'PATCH',
+                headers: {
+                    'content-type': 'application/json;charset=UTF-8',
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            return await query.json();
+        } catch (error) {
+            return error;
+        }
+    }
+
+	static async removeSoft(id: number) {
+		try {
+			const token = store.getState().authToken;
+			const query: Response = await fetch(`${CategoriesServices.API_URL}/categories/${id}/soft`, {
+				method: 'DELETE',
+				headers: {
+					'content-type': 'application/json;charset=UTF-8',
+					Authorization: `Bearer ${token}`
+				}
+			});
+			return await query.json();
+		} catch (error) {
+			return error;
+		}
+	}
+
+    static async remove(id: number) {
+		try {
+			const token = store.getState().authToken;
+			const query: Response = await fetch(`${CategoriesServices.API_URL}/categories/${id}`, {
+				method: 'DELETE',
 				headers: {
 					'content-type': 'application/json;charset=UTF-8',
 					Authorization: `Bearer ${token}`

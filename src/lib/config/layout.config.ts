01c45ebeb6fb37acf7d145945be3141ec04d2184
alanloffler <alanmatiasloffler@gmail.com>
2024-05-18 13:53:05 -0300

    Layout config file created

---
+++ src/lib/config/layout.config.ts
@@ -0,0 +1,22 @@
+export const LayoutConfig = {
+	appName: 'Remtie',
+	sidebar: {
+		menu: {
+            allProducts: 'Propiedades',
+			dashboard: 'Tablero',
+			products: 'Editar Propiedades',
+			settings: 'Configuraciones',
+			users: 'Usuarios'
+		}
+	},
+	footer: {
+		center: 'Remtie',
+		left: '© 2024 Remtie',
+		right: 'Dev by'
+	},
+	userLogged: {
+		edit: 'Editar',
+		logout: 'Cerrar sesión',
+		profile: 'Perfil'
+	}
+};

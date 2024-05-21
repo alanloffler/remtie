8b2cff48b509eef8074cae67837e6d761fbfabe5
alanloffler <alanmatiasloffler@gmail.com>
2024-05-21 13:37:42 -0300

    Added option for latestProperties (all or owner)

--- src/services/dashboard.services.ts
+++ src/services/dashboard.services.ts
@@ -20,10 +20,12 @@ export class DashboardServices {
 		}
 	}
 
-	static async getLatestProperties(id: number) {
+	static async getLatestProperties(id: number, owner: string) {
+		let url: string = '';
+		owner === '0' ? (url = 'latest') : (url = 'latestActiveUser');
 		try {
 			const token: string = store.getState().authToken;
-			const request: Response = await fetch(`${DashboardServices.API_URL}/properties/${id}/latest`, {
+			const request: Response = await fetch(`${DashboardServices.API_URL}/properties/${id}/${url}`, {
 				method: 'GET',
 				headers: {
 					'content-type': 'application/json;charset=UTF-8',
@@ -35,22 +37,6 @@ export class DashboardServices {
 			return error;
 		}
 	}
-
-	// static async countByCreator() {
-	// 	try {
-	// 		const token: string = store.getState().authToken;
-	// 		const request: Response = await fetch(`${DashboardServices.API_URL}/properties/dashboard/countByCreator`, {
-	// 			method: 'GET',
-	// 			headers: {
-	// 				'content-type': 'application/json;charset=UTF-8',
-	// 				Authorization: 'Bearer ' + token
-	// 			}
-	// 		});
-	// 		return await request.json();
-	// 	} catch (error) {
-	// 		return error;
-	// 	}
-	// }
 
 	static async getHeaderData() {
 		try {

fef450d9723d60be8a387ebdf9ff6a9fd5dd864b
alanloffler <alanmatiasloffler@gmail.com>
2024-05-21 18:12:36 -0300

    Added animation

--- src/components/settings/SettingsUsers.tsx
+++ src/components/settings/SettingsUsers.tsx
@@ -1,10 +1,10 @@
-import UsersRowsPerPage from '@/components/settings/users/UsersRowsPerPage';
+// App
 import UsersRoles from '@/components/settings/users/UsersRoles';
-
+import UsersRowsPerPage from '@/components/settings/users/UsersRowsPerPage';
 // React component
 function SettingsUsers() {
 	return (
-		<div className='mb-8 flex flex-col md:flex-row md:gap-6'>
+		<div className='mb-8 flex animate-fadeIn flex-col md:flex-row md:gap-6'>
 			<div className='w-full p-2 md:w-1/2'>
 				<div className='space-y-4'>
                     <UsersRoles />

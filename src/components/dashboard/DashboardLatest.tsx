21c8e9f7f05df97c38a8b0c5104bcf4216566853
alanloffler <alanmatiasloffler@gmail.com>
2024-05-21 14:03:08 -0300

    Styles changed

--- src/components/dashboard/DashboardLatest.tsx
+++ src/components/dashboard/DashboardLatest.tsx
@@ -74,13 +74,13 @@ function DashboardLatest({ limit }: { limit: number }) {
 				) : (
 					<div className='w-full'>
 						<ul className='flex flex-col gap-2'>
-							{latestProperties.map((property: IProperty) => {
+							{latestProperties.map((property: IProperty, index: number) => {
 								return (
-									<li key={property.id} className='rounded-md border bg-slate-100/20 px-2 py-2 text-slate-700 transition-all hover:cursor-pointer hover:bg-slate-200/30 hover:text-black'>
+									<li key={property.id} className={`${index % 2 === 0 ? 'bg-gray-100/60' : ''} rounded-md bg-slate-100/20 px-2 py-2 text-slate-700 transition-all hover:cursor-pointer hover:bg-slate-200/30 hover:text-black`}>
 										<Link to={`${APP_URL}/${property.id}`} className='flex w-full items-center'>
 											<span className='rounded-sm bg-slate-200/70 px-1.5 py-1 text-xs font-bold uppercase leading-tight'>{`COD-${property.id < 10 ? '0' + property.id : property.id}`}</span>
 											<span className='px-2 text-xs font-semibold'>{`${capitalize(property.type)} - ${capitalize(property.business_type)}`}</span>
-											<span className='text-xs'>{truncateText(property.title, 40 - property.type.length - property.business_type.length)}</span>
+											<span className='hidden text-xs md:inline-flex lg:inline-flex'>{truncateText(property.title, 40 - property.type.length - property.business_type.length)}</span>
 											<span className='ml-auto flex items-center gap-1 px-2 text-xs'>
 												<CalendarPlus className='h-4 w-4 text-emerald-500' />
 												{localeDate(property.created_at)}

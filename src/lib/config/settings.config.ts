5dcea2880d8b200d0d8463040ab71b30c3408e57
alanloffler <alanmatiasloffler@gmail.com>
2024-05-21 18:13:32 -0300

    Property added

--- src/lib/config/settings.config.ts
+++ src/lib/config/settings.config.ts
@@ -1,37 +1,41 @@
 export const SettingsConfig = {
-	title: 'Configuraciones generales',
-	sections: {
-		businessType: 'Tipo de negocio',
-		categoriesType: 'Tipo de categoría',
-		defaultView: {
-			title: 'Vista predeterminada',
-			subtitle: 'La vista actual es'
-		},
-        rowsPerPage: {
-            title: 'Opciones de paginación',
-            subtitle1: 'Elementos por página: ',
-            subtitle2: 'Menú de opciones'
-        },
-        roles: {
-            title: 'Roles de usuarios'
-        }
+	buttons: {
+		back: 'Volver',
+		save: 'Guardar'
 	},
 	common: {
 		color: 'Color (#000000)',
 		name: 'Nombre',
-        no_data: 'Sin datos',
+		no_data: 'Sin datos',
 		plural: 'Plural',
 		property: 'Propiedades',
-        select: 'Seleccionar',
-        title: 'Título',
-        value: 'Valor',
+		select: 'Seleccionar',
+		title: 'Título',
+		value: 'Valor',
 		views: {
 			card: 'Tarjeta',
 			list: 'Lista'
 		}
 	},
-	buttons: {
-		back: 'Volver',
-		save: 'Guardar'
-	}
+	sections: {
+		businessType: 'Tipo de negocio',
+		categoriesType: 'Tipo de categoría',
+		dashboardLimit: {
+			title: 'Límite de propiedades'
+		},
+		defaultView: {
+			title: 'Vista predeterminada',
+			subtitle: 'La vista actual es'
+		},
+		rowsPerPage: {
+			title: 'Opciones de paginación',
+			subtitle1: 'Elementos por página: ',
+			subtitle2: 'Menú de opciones'
+		},
+		roles: {
+			title: 'Roles de usuarios'
+		}
+	},
+	showing: ['Mostrando', 'propiedades'],
+	title: 'Configuraciones generales'
 };

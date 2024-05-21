c5b01eab3b4494ba0a23c6ff456a2845aee22585
alanloffler <alanmatiasloffler@gmail.com>
2024-05-21 13:39:54 -0300

    New dashboard config file

---
+++ src/lib/config/dashboard.config.ts
@@ -0,0 +1,14 @@
+export const DashboardConfig = {
+    components: {
+        latest: {
+            title: 'Propiedades recientes',
+        }
+    },
+    dropdown: {
+        allProducts: 'Todas',
+        ownerProducts: 'Tuyas'
+    },
+    noProducts: 'Aún no hay propiedades para mostrar.',
+    owner: 'de tu autoría',
+    seeMore: 'Ver más'
+};

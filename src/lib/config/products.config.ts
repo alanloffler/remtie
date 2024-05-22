0dc4d6aff8aeea086a2975f9aa35d837d61b6389
alanloffler <alanmatiasloffler@gmail.com>
2024-05-22 11:47:20 -0300

    Update values

--- src/lib/config/products.config.ts
+++ src/lib/config/products.config.ts
@@ -11,11 +11,56 @@ export const ProductsConfig = {
         propertyRestore: 'La propiedad va a ser restaurada de la base de datos.',
         title: '¿Estás realmente seguro?',
     },
+    imageDialog: {
+        imageDelete: 'La imágen va a ser eliminada permanentemente de la base de datos.',
+        imageSoftDelete: 'La imágen va a ser eliminada de la base de datos.',
+        imageRestore: 'La imágen va a ser restaurada de la base de datos.'
+    },
     filters: {
         byCreator: 'Filtrar por creadores'
     },
+    form: {
+        active: 'Activo',
+        addImages: 'Agregar imágenes',
+        address: 'Dirección',
+        category: 'Categoría',
+        chooseImage: 'Seleccionar imágen',
+        city: 'Ciudad',
+        goToProperty: 'Ver la propiedad que creaste',
+        longDescription: 'Descripción extendida',
+        price: 'Precio',
+        shortDescription: 'Descripción breve',
+        state: 'Provincia',
+        street: 'Calle',
+        title: 'Título',
+        type: 'Tipo',
+        zip: 'Código Postal'
+    },
     headers: ['#', 'Tipo', 'Categoría', 'Título', 'Ciudad', 'Precio', 'Acciones'],
 	noResults: 'Sin resultados',
+    pages: {
+        create: {
+            dialog: {
+                title: '¿Estás realmente seguro?',
+                subtitle: 'Esta acción es imposible de revertir.',
+                message: 'La imágen de la propiedad se eliminará permanentemente de la base de datos.'
+            },
+            subtitle: 'Descripción',
+            title: "Crear Propiedad",
+        },
+        update: {
+            title: 'Editar Propiedad'
+        },
+        view: {
+            deleted: 'Eliminado',
+            lastUpdate: 'Última actualización realizada el',
+            sentence: {
+                goToProperty: 'Ver la propiedad',
+                creation: ['Creado por ', ' el ', ],
+                location: ['Ubicado en ', ', '],
+            }
+        }
+    },
 	pagination: {
 		page: 'Página',
 		of: 'de',

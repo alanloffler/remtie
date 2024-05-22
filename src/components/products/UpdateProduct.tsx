4e5e9070d4e3f9dbf7f7c7a61ad0c2bd1f376595
alanloffler <alanmatiasloffler@gmail.com>
2024-05-22 20:15:39 -0300

    Added accordion for image manegement

--- src/components/products/UpdateProduct.tsx
+++ src/components/products/UpdateProduct.tsx
@@ -1,6 +1,7 @@
 // Icons: Lucide (https://lucide.dev/)
 import { ArrowLeft, Trash2, BadgeX, CheckCircle } from 'lucide-react';
 // UI: Shadcn-ui (https://ui.shadcn.com/)
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
@@ -25,7 +26,7 @@ import { IProperty } from '@/lib/interfaces/property.interface';
 import { ImageServices } from '@/services/image.services';
 import { ProductsConfig } from '@/lib/config/products.config';
 import { ProductsServices } from '@/services/products.services';
-import { ReactElement, useEffect, useState } from 'react';
+import { FormEvent, ReactElement, useEffect, useState } from 'react';
 import { Roles } from '@/lib/constants';
 import { getImageURL } from '@/lib/image-util';
 import { imageFormSchema } from '@/lib/schemas/image.schema';
@@ -46,6 +47,7 @@ function UpdateProduct() {
 	const [businessKey, setBusinessKey] = useState<number>(0);
 	const [categories, setCategories] = useState<ICategory[]>([]);
 	const [categoriesKey, setCategoriesKey] = useState<number>(0);
+	const [chosenImage, setChosenImage] = useState<string>('');
 	const [dialogAction, setDialogAction] = useState<string>('');
 	const [images, setImages] = useState<IImage[]>([]);
 	const [imageDialog, setImageDialog] = useState<IDialog>({ id: 0, name: '', title: '', subtitle: '', message: <span></span> });
@@ -151,6 +153,10 @@ function UpdateProduct() {
 			if (response instanceof Error) toast({ title: 'Error', description: '500 Internal Server Error | ' + response.message, variant: 'destructive', duration: 5000 });
 		});
 	}
+
+	function handleInputChange(event: FormEvent<HTMLInputElement>) {
+		setChosenImage(event.currentTarget.value.split('\\')[2]);
+	}
 	// #endregion
 	// #region Dialog
 	function handleDialog(image: IImage, action: string) {
@@ -203,7 +209,7 @@ function UpdateProduct() {
 			if (response.statusCode > 399) toast({ title: response.statusCode, description: response.message, variant: 'destructive', duration: 5000 });
 			if (response instanceof Error) toast({ title: 'Error', description: '500 Internal Server Error | ' + response.message, variant: 'destructive', duration: 5000 });
 		});
-        setOpenDialog(false);
+		setOpenDialog(false);
 	}
 
 	async function remove(id: number) {
@@ -214,7 +220,7 @@ function UpdateProduct() {
 			if (response.statusCode > 399) toast({ title: response.statusCode, description: response.message, variant: 'destructive', duration: 5000 });
 			if (response instanceof Error) toast({ title: 'Error', description: '500 Internal Server Error | ' + response.message, variant: 'destructive', duration: 5000 });
 		});
-        setOpenDialog(false);
+		setOpenDialog(false);
 	}
 
 	async function restore(id: number) {
@@ -225,7 +231,7 @@ function UpdateProduct() {
 			if (response.statusCode > 399) toast({ title: response.statusCode, description: response.message, variant: 'destructive', duration: 5000 });
 			if (response instanceof Error) toast({ title: 'Error', description: '500 Internal Server Error | ' + response.message, variant: 'destructive', duration: 5000 });
 		});
-        setOpenDialog(false);
+		setOpenDialog(false);
 	}
 
 	function switchActive(check: boolean) {
@@ -237,7 +243,7 @@ function UpdateProduct() {
 	}
 	// #endregion
 	return (
-		<main className='flex-1 overflow-y-auto'>
+		<main className='flex-1 animate-fadeIn overflow-y-auto'>
 			<div className='flex flex-row items-center justify-between px-8 pt-8'>
 				<h1 className='text-2xl font-normal text-slate-600'>{ProductsConfig.pages.update.title}</h1>
 				<Button variant='ghost' size='sm' onClick={() => navigate(-1)}>
@@ -248,7 +254,7 @@ function UpdateProduct() {
 			<div className='mt-6 flex flex-col items-center justify-center px-8'>
 				<Card className='flex w-full flex-row py-8 md:w-[500px] lg:w-[650px]'>
 					<CardContent className='mx-0 w-full p-0'>
-                        {/* SECTION: Property form */}
+						{/* SECTION: Property form */}
 						<Form {...propertyForm}>
 							<form onSubmit={propertyForm.handleSubmit(handleSubmitProduct)} className='space-y-8'>
 								<div className='container mx-auto space-y-4'>
@@ -474,133 +480,146 @@ function UpdateProduct() {
 								</div>
 							</form>
 						</Form>
+						{/* working here */}
+						<div className='px-8'>
+							<Accordion type='single' defaultValue='item-1' collapsible>
+								<AccordionItem value='item-1' className='border-none'>
+									<AccordionTrigger className='justify-start gap-2 pb-0 pt-4'>{ProductsConfig.form.addImages}</AccordionTrigger>
+									<AccordionContent className='py-0'>
+										{/* SECTION: Images list */}
+										<div className='grid gap-4 pt-6'>
+											{images.map((img, i) => {
+												return (
+													<Card key={img.id} className='bg-slate-100/50 px-2 py-2'>
+														<div className='flex flex-row place-items-center justify-between'>
+															<div className='flex h-6 flex-row space-x-2'>
+																<img src={getImageURL(img.name)} />
+																<h2 className='flex flex-row place-items-center text-xs font-medium text-slate-900'># {i + 1}</h2>
+															</div>
+															<div className='hidden flex-row text-xs font-light text-slate-400 xs:block md:block lg:block'>{img.name}</div>
+															<div className='flex flex-row items-center gap-2'>
+																{img.deletedAt === null ? (
+																	<Button
+																		onClick={() => {
+																			handleDialog(img, 'removeSoft');
+																		}}
+																		variant='ghost'
+																		size='miniIcon'
+																		className='h-7 w-7 rounded-full border bg-white text-slate-400/70 shadow-sm hover:bg-white hover:text-rose-500'>
+																		<Trash2 className='h-4 w-4' />
+																	</Button>
+																) : (
+																	<Button
+																		onClick={() => {
+																			handleDialog(img, 'restore');
+																		}}
+																		variant='ghost'
+																		size='miniIcon'
+																		className='h-7 w-7 rounded-full border bg-white text-slate-400/70 shadow-sm hover:bg-white hover:text-emerald-400'>
+																		<CheckCircle className='h-4 w-4' />
+																	</Button>
+																)}
+																{store.getState().role === Roles.ADMIN && (
+																	<Button
+																		onClick={() => {
+																			handleDialog(img, 'remove');
+																		}}
+																		variant='ghost'
+																		size='miniIcon'
+																		className='h-7 w-7 rounded-full border bg-white text-slate-400/70 shadow-sm hover:bg-white hover:text-rose-500'>
+																		<BadgeX className='h-5 w-5' strokeWidth='1.5' />
+																	</Button>
+																)}
+															</div>
+														</div>
+													</Card>
+												);
+											})}
+										</div>
+										{/* SECTION: File input */}
+										{store.getState().userId === property.created_by && (
+											<FormProvider {...imageForm}>
+												<form onSubmit={imageForm.handleSubmit(handleSubmitImage)}>
+													<div className='mt-6 grid w-full grid-cols-1 items-center gap-1.5'>
+														<FormField
+															control={imageForm.control}
+															name='file'
+															render={() => (
+																<FormItem className='grid grid-cols-1 items-center justify-between space-y-4 md:grid-cols-3 md:space-y-0'>
+																	<div className='flex flex-row items-center gap-2 md:col-span-2'>
+																		<FormLabel className='h-fit rounded-md border bg-slate-100/70 p-2 font-semibold text-slate-500 hover:cursor-pointer hover:bg-slate-100'>{ProductsConfig.form.chooseImage}</FormLabel>
+																		<span className='text-md flex flex-row font-light text-slate-600'>{chosenImage}</span>
+																		<FormControl>
+																			<Input {...imageForm.register('file')} name='file' type='file' accept='image/*' className='m-0 h-0 w-0 p-0 opacity-0 hover:cursor-pointer' onChange={(e) => handleInputChange(e)} />
+																		</FormControl>
+																	</div>
+																	<div className='flex flex-row place-content-end gap-4 md:col-span-1'>
+																		<Button
+																			variant='ghost'
+																			size='sm'
+																			onClick={(e) => {
+																				e.preventDefault();
+																				imageForm.reset();
+																				setChosenImage('');
+																			}}
+																			className='w-auto px-2 text-xs'>
+																			{ButtonsConfig.actions.cancel}
+																		</Button>
+																		<Button variant='default' size='sm' className='w-auto border-slate-400 bg-slate-400 px-2 text-xs hover:border-slate-500 hover:bg-slate-500' type='submit'>
+																			{ButtonsConfig.actions.save}
+																		</Button>
+																	</div>
+																	<FormMessage />
+																</FormItem>
+															)}
+														/>
+													</div>
+												</form>
+											</FormProvider>
+										)}
+									</AccordionContent>
+								</AccordionItem>
+							</Accordion>
+						</div>
 					</CardContent>
 				</Card>
-				<div className='my-8 flex w-full flex-col md:w-[500px] lg:w-[650px]'>
-					<h1 className='font-semibold text-slate-800'>{ProductsConfig.form.addImages}</h1>
-					<Separator className='mt-2' />
-                    {/* SECTION: Images list */}
-					<div className='mt-6 grid gap-4'>
-						{images.map((img, i) => {
-							return (
-								<Card key={img.id} className='px-2 py-2'>
-									<div className='flex flex-row place-items-center justify-between'>
-										<div className='flex h-5 flex-row'>
-											<img src={getImageURL(img.name)} />
-											<h2 className='flex flex-row place-items-center pl-3 text-xs font-medium text-slate-900'># {i + 1}</h2>
-										</div>
-										<div className='hidden flex-row text-xs font-light text-slate-400 xs:block md:block lg:block'>{img.name}</div>
-										<div className='flex flex-row gap-2'>
-											{img.deletedAt === null ? (
-												<Button
-													onClick={() => {
-														handleDialog(img, 'removeSoft');
-													}}
-													variant='ghost'
-													size='miniIcon'
-													className='rounded-full border bg-white text-slate-400/70 shadow-sm hover:bg-white hover:text-rose-500'>
-													<Trash2 className='h-4 w-4' />
-												</Button>
-											) : (
-												<Button
-													onClick={() => {
-														handleDialog(img, 'restore');
-													}}
-													variant='ghost'
-													size='miniIcon'
-													className='rounded-full border bg-white text-slate-400/70 shadow-sm hover:bg-white hover:text-emerald-400'>
-													<CheckCircle className='h-4 w-4' />
-												</Button>
-											)}
-											{store.getState().role === Roles.ADMIN && (
-												<Button
-													onClick={() => {
-                                                        handleDialog(img, 'remove');
-													}}
-													variant='ghost'
-													size='miniIcon'
-													className='rounded-full border bg-white text-slate-400/70 shadow-sm hover:bg-white hover:text-rose-500'>
-													<BadgeX className='h-5 w-5' strokeWidth='1.5' />
-												</Button>
-											)}
-										</div>
-									</div>
-								</Card>
-							);
-						})}
-						<Dialog open={openDialog} onOpenChange={setOpenDialog}>
-							<DialogContent>
-								<DialogHeader>
-									<DialogTitle>{imageDialog.title}</DialogTitle>
-									<DialogDescription>{imageDialog.subtitle}</DialogDescription>
-								</DialogHeader>
-								<section className='text-sm font-normal'>{imageDialog.message}</section>
-								<DialogFooter>
-									<div className='mt-6 flex flex-row gap-4'>
-										<Button variant='ghost' onClick={() => setOpenDialog(false)}>
-											{ButtonsConfig.actions.cancel}
-										</Button>
-										{dialogAction === 'removeSoft' && (
-											<Button variant='delete' onClick={() => removeSoft(imageDialog.id)}>
-												{ButtonsConfig.actions.delete}
-											</Button>
-										)}
-										{dialogAction === 'restore' && (
-											<Button variant='default' onClick={() => restore(imageDialog.id)}>
-												{ButtonsConfig.actions.restore}
-											</Button>
-										)}
-										{dialogAction === 'remove' && (
-											<Button variant='delete' onClick={() => remove(imageDialog.id)}>
-												{ButtonsConfig.actions.delete}
-											</Button>
-										)}
-									</div>
-								</DialogFooter>
-							</DialogContent>
-						</Dialog>
-					</div>
-                    {/* SECTION: Image form */}
-					{store.getState().userId === property.created_by && (
-						<FormProvider {...imageForm}>
-							<form onSubmit={imageForm.handleSubmit(handleSubmitImage)}>
-								<div className='mt-8 grid w-full max-w-sm items-center gap-1.5'>
-									<FormField
-										control={imageForm.control}
-										name='file'
-										render={() => (
-											<FormItem className='w-full space-y-1'>
-												<FormLabel className='font-semibold text-slate-500'>Agregar imágen</FormLabel>
-												<FormControl>
-													<Input {...imageForm.register('file')} name='file' type='file' accept='image/*' className='w-full hover:cursor-pointer' />
-												</FormControl>
-												<FormMessage />
-											</FormItem>
-										)}
-									/>
-								</div>
-								<div className='mt-8 flex flex-row justify-end space-x-4'>
-									<Button
-										variant='ghost'
-										onClick={(e) => {
-											e.preventDefault();
-											imageForm.reset();
-										}}>
-										{ButtonsConfig.actions.cancel}
-									</Button>
-									<Button variant='default' size='default' className='w-auto' type='submit'>
-										{ButtonsConfig.actions.save}
-									</Button>
-								</div>
-							</form>
-						</FormProvider>
-					)}
-				</div>
 				<section className='py-6'>
 					<Button onClick={() => navigate(`${APP_URL}/productos/${propertyId}`)} variant='secondary' size='sm' className='w-auto border bg-slate-200 hover:bg-slate-200/70'>
 						{ProductsConfig.pages.view.sentence.goToProperty}
 					</Button>
 				</section>
+				{/* SECTION: Dialog */}
+				<Dialog open={openDialog} onOpenChange={setOpenDialog}>
+					<DialogContent>
+						<DialogHeader>
+							<DialogTitle>{imageDialog.title}</DialogTitle>
+							<DialogDescription>{imageDialog.subtitle}</DialogDescription>
+						</DialogHeader>
+						<section className='text-sm font-normal'>{imageDialog.message}</section>
+						<DialogFooter>
+							<div className='mt-6 flex flex-row gap-4'>
+								<Button variant='ghost' onClick={() => setOpenDialog(false)}>
+									{ButtonsConfig.actions.cancel}
+								</Button>
+								{dialogAction === 'removeSoft' && (
+									<Button variant='delete' onClick={() => removeSoft(imageDialog.id)}>
+										{ButtonsConfig.actions.delete}
+									</Button>
+								)}
+								{dialogAction === 'restore' && (
+									<Button variant='default' onClick={() => restore(imageDialog.id)}>
+										{ButtonsConfig.actions.restore}
+									</Button>
+								)}
+								{dialogAction === 'remove' && (
+									<Button variant='delete' onClick={() => remove(imageDialog.id)}>
+										{ButtonsConfig.actions.delete}
+									</Button>
+								)}
+							</div>
+						</DialogFooter>
+					</DialogContent>
+				</Dialog>
 			</div>
 		</main>
 	);

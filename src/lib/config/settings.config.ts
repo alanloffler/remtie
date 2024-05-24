061b57681dc1edc0130809b5672f02f8c1b0f4ed
alanloffler <alanmatiasloffler@gmail.com>
2024-05-24 15:13:38 -0300

    Config added

--- src/lib/config/settings.config.ts
+++ src/lib/config/settings.config.ts
@@ -17,9 +17,34 @@ export const SettingsConfig = {
 			list: 'Lista'
 		}
 	},
+    dialog: {
+        city: {
+            cityDelete: 'La ciudad va a ser eliminada permanentemente de la base de datos.',
+            citySoftDelete: 'La ciudad va a ser eliminada de la base de datos.',
+            cityRestore: 'La ciudad va a ser restaurada de la base de datos.',
+        },
+        impossibleRevertion: 'Esta acción es posible de revertir por el administrador',
+        possibleRevertion: 'Esta acción es posible de revertir por el administrador',
+        title: '¿Estás realmente seguro?',
+    },
 	sections: {
 		businessType: 'Tipo de negocio',
 		categoriesType: 'Tipo de categoría',
+		cities: {
+			form: {
+				cityTitle: 'Crear ciudad',
+                cityPlaceholder: 'Nombre',
+                editCityTitle: 'Editar ciudad',
+				stateTitle: 'Crear provincia',
+                zipPlaceholder: 'Código Postal',
+			},
+			list: {
+				cities: 'Ciudades',
+				states: 'Provincias'
+			},
+			tableHeaders: ['#', 'Ciudad', 'Provincia', 'CP', 'Estado'],
+			title: 'Gestión de ciudades'
+		},
 		dashboardLimit: {
 			title: 'Límite de propiedades'
 		},

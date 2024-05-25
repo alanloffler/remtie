002fad20072bbfb1aeb8152094778a0804ba36ae
alanloffler <alanmatiasloffler@gmail.com>
2024-05-25 17:33:46 -0300

    Update to capitalize more than one word

--- src/hooks/useCapitalize.ts
+++ src/hooks/useCapitalize.ts
@@ -1,11 +1,16 @@
+import { useCallback } from 'react';
+
 function useCapitalize() {
-	return (word: string | undefined) => {
-		if (word) {
-			return word.charAt(0).toUpperCase() + word.slice(1);
+	return useCallback((sentence: string | undefined) => {
+		if (sentence) {
+			return sentence
+				.split(' ')
+				.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+				.join(' ');
 		} else {
-			return 'No date provided';
+			return 'No data provided';
 		}
-	};
+	}, []);
 }
 
 export { useCapitalize };

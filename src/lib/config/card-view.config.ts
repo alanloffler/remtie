580c659fce1dd39fea8c3f782b83f13504c4535c
alanloffler <alanmatiasloffler@gmail.com>
2024-05-26 22:14:43 -0300

    CardView config file created

---
+++ src/lib/config/card-view.config.ts
@@ -0,0 +1,28 @@
+export const CardViewConfig = {
+    dialog: {
+        impossibleRevertion: 'Esta acción es imposible de revertir',
+		possibleRevertion: 'Esta acción es posible de revertir por el administrador',
+		propertyDelete: 'La propiedad va a ser eliminada permanentemente de la base de datos.',
+		propertySoftDelete: 'La propiedad va a ser eliminada de la base de datos.',
+		propertyRestore: 'La propiedad va a ser restaurada de la base de datos.',
+		title: '¿Estás realmente seguro?'
+	},
+    property: {
+        status: {
+            active: 'Activo',
+            inactive: 'Inactivo'
+        }
+    },
+    tooltip: {
+        delete: 'Eliminar',
+        deletePermanent: 'Eliminar permanente',
+        details: 'Ver detalle',
+        edit: 'Editar',
+        restore: 'Restaurar',
+    },
+    words: {
+        image: 'imágen',
+        images: 'imagenes',
+        googleMaps: 'Mapa'
+    }
+}

896109a522ac8de1eef7483d8407a2ac988479e9
alanloffler <alanmatiasloffler@gmail.com>
2024-05-26 22:15:00 -0300

    Added configs

--- src/lib/config/products.config.ts
+++ src/lib/config/products.config.ts
@@ -3,68 +3,68 @@ export const ProductsConfig = {
 		admin: 'Aún no hay propiedades creadas por los usuarios o por vos.',
 		user: 'No hay propiedades creadas por vos'
 	},
-    dialog: {
-        impossibleRevertion: 'Esta acción es posible de revertir por el administrador',
-        possibleRevertion: 'Esta acción es posible de revertir por el administrador',
-        propertyDelete: 'La propiedad va a ser eliminada permanentemente de la base de datos.',
-        propertySoftDelete: 'La propiedad va a ser eliminada de la base de datos.',
-        propertyRestore: 'La propiedad va a ser restaurada de la base de datos.',
-        title: '¿Estás realmente seguro?',
-    },
-    imageDialog: {
-        imageDelete: 'La imágen va a ser eliminada permanentemente de la base de datos.',
-        imageSoftDelete: 'La imágen va a ser eliminada de la base de datos.',
-        imageRestore: 'La imágen va a ser restaurada de la base de datos.'
-    },
-    filters: {
-        byCreator: 'Filtrar por creadores'
-    },
-    form: {
-        active: 'Activo',
-        addImages: 'Agregar imágenes',
-        address: 'Dirección',
-        category: 'Categoría',
-        chooseImage: 'Seleccionar imágen',
-        city: 'Ciudad',
-        goToProperty: 'Ver la propiedad que creaste',
-        longDescription: 'Descripción extendida',
-        price: 'Precio',
-        shortDescription: 'Descripción breve',
-        state: 'Provincia',
-        street: 'Calle',
-        title: 'Título',
-        type: 'Tipo',
-        zip: 'Código Postal'
-    },
-    headers: ['#', 'Tipo', 'Categoría', 'Título', 'Ciudad', 'Precio', 'Acciones'],
+	dialog: {
+		impossibleRevertion: 'Esta acción es imposible de revertir',
+		possibleRevertion: 'Esta acción es posible de revertir por el administrador',
+		propertyDelete: 'La propiedad va a ser eliminada permanentemente de la base de datos.',
+		propertySoftDelete: 'La propiedad va a ser eliminada de la base de datos.',
+		propertyRestore: 'La propiedad va a ser restaurada de la base de datos.',
+		title: '¿Estás realmente seguro?'
+	},
+	imageDialog: {
+		imageDelete: 'La imágen va a ser eliminada permanentemente de la base de datos.',
+		imageSoftDelete: 'La imágen va a ser eliminada de la base de datos.',
+		imageRestore: 'La imágen va a ser restaurada de la base de datos.'
+	},
+	filters: {
+		byCreator: 'Filtrar por creadores'
+	},
+	form: {
+		active: 'Activo',
+		addImages: 'Agregar imágenes',
+		address: 'Dirección',
+		category: 'Categoría',
+		chooseImage: 'Seleccionar imágen',
+		city: 'Ciudad',
+		goToProperty: 'Ver la propiedad que creaste',
+		longDescription: 'Descripción extendida',
+		price: 'Precio',
+		shortDescription: 'Descripción breve',
+		state: 'Provincia',
+		street: 'Calle',
+		title: 'Título',
+		type: 'Tipo',
+		zip: 'Código Postal'
+	},
+	headers: ['#', 'Tipo', 'Categoría', 'Título', 'Ciudad', 'Precio', 'Acciones'],
 	noResults: 'Sin resultados',
-    pages: {
-        create: {
-            dialog: {
-                title: '¿Estás realmente seguro?',
-                subtitle: 'Esta acción es imposible de revertir.',
-                message: 'La imágen de la propiedad se eliminará permanentemente de la base de datos.'
-            },
-            subtitle: 'Descripción',
-            title: "Crear Propiedad",
-        },
-        update: {
-            title: 'Editar Propiedad'
-        },
-        view: {
-            deleted: 'Eliminado',
-            lastUpdate: 'Última actualización realizada el',
-            sentence: {
-                goToProperty: 'Ver la propiedad',
-                creation: ['Creada por ', ' el ', ],
-                location: ['Ubicada en ', ', '],
-            }
-        }
-    },
+	pages: {
+		create: {
+			dialog: {
+				title: '¿Estás realmente seguro?',
+				subtitle: 'Esta acción es imposible de revertir.',
+				message: 'La imágen de la propiedad se eliminará permanentemente de la base de datos.'
+			},
+			subtitle: 'Descripción',
+			title: 'Crear Propiedad'
+		},
+		update: {
+			title: 'Editar Propiedad'
+		},
+		view: {
+			deleted: 'Eliminado',
+			lastUpdate: 'Última actualización realizada el',
+			sentence: {
+				goToProperty: 'Ver la propiedad',
+				creation: ['Creada por ', ' el '],
+				location: ['Ubicada en ', ', ']
+			}
+		}
+	},
 	pagination: {
 		page: 'Página',
 		of: 'de',
 		rowsPerPage: 'Filas por página'
 	},
-    search: 'Buscar por ciudad'
+	search: 'Buscar por ciudad'
 };

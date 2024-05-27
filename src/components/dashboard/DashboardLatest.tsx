0910b1227de312999423a05a40b4d0c8b50a57dc
alanloffler <alanmatiasloffler@gmail.com>
2024-05-27 00:25:09 -0300

    Route fixed for latest properties

--- src/components/dashboard/DashboardLatest.tsx
+++ src/components/dashboard/DashboardLatest.tsx
@@ -88,7 +88,7 @@ function DashboardLatest() {
 							{latestProperties.map((property: IProperty, index: number) => {
 								return (
 									<li key={property.id} className={`${index % 2 === 0 ? 'bg-gray-100/60' : ''} rounded-md bg-slate-100/20 px-2 py-2 text-slate-700 transition-all hover:cursor-pointer hover:bg-slate-200/30 hover:text-black`}>
-										<Link to={`${APP_URL}/${property.id}`} className='flex w-full items-center'>
+										<Link to={`${APP_URL}/productos/${property.id}`} className='flex w-full items-center'>
 											<span className='rounded-sm bg-slate-200/70 px-1.5 py-1 text-xs font-bold uppercase leading-tight'>{`COD-${property.id < 10 ? '0' + property.id : property.id}`}</span>
 											<span className='px-2 text-xs font-semibold'>{`${capitalize(property.type)} - ${capitalize(property.business_type)}`}</span>
 											<span className='hidden text-xs md:inline-flex lg:inline-flex'>{truncateText(property.title, 40 - property.type.length - property.business_type.length)}</span>
@@ -102,7 +102,7 @@ function DashboardLatest() {
 							})}
 						</ul>
 						<div className='mt-4 flex flex-row justify-end'>
-							<Button onClick={() => navigate(`${APP_URL + propertiesUrl}`)} variant='link' size='sm' className='h-8 text-xs font-semibold text-slate-500'>
+							<Button onClick={() => navigate(`${APP_URL}${propertiesUrl}`)} variant='link' size='sm' className='h-8 text-xs font-semibold text-slate-500'>
 								{DashboardConfig.seeMore}
 							</Button>
 						</div>

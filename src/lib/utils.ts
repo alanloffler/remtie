4eaf591fc03d1ad795e00bcd8a75be81893329fe
alanloffler <alanmatiasloffler@gmail.com>
2024-05-26 22:14:16 -0300

    Added confirmMapExistence (Google Map)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,12 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { IMarker } from "./interfaces/google-map.interface";
 import { IUser } from "@/lib/interfaces/user.interface";
-
+import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx";
+// Shadcn-ui
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
-
+// User
 export const emptyUser: IUser = {
     id: 0,
     name: "",
@@ -13,3 +14,8 @@ export const emptyUser: IUser = {
     email: "",
     role: "",
 };
+// Google Map
+export function confirmMapExistence(marker: IMarker) {
+    if ((marker.lat === 0 && marker.lng === 0) || marker.key === '' || marker.key === null || marker.zoom === 0) return false;
+    return true;
+}

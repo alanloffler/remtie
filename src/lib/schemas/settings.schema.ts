c9392b1f2052747377b71e1697ad5ba1c2b38dbe
alanloffler <alanmatiasloffler@gmail.com>
2024-05-27 22:35:00 -0300

    Added schema for map settings

--- src/lib/schemas/settings.schema.ts
+++ src/lib/schemas/settings.schema.ts
@@ -5,3 +5,16 @@ export const settingsSchema = z.object({
 		message: 'El valor debe poseer al menos 1 caracter'
 	})
 });
+
+export const mapSettingsSchema = z.object({
+	lat: z.string().min(1, {
+		message: 'El valor debe poseer al menos 1 caracter'
+	}),
+	lng: z.string().min(1, {
+		message: 'El valor debe poseer al menos 1 caracter'
+	}),
+    zoom: z.coerce.number({ invalid_type_error: 'El zoom debe ser un número' }).positive(),
+    mapType: z.string().min(1, {
+		message: 'Debes elegir un tipo de mapa'
+	}),
+});

3aaa0062e01d02485796d854e8ee57b1f5dc5437
alanloffler <alanmatiasloffler@gmail.com>
2024-05-27 22:34:27 -0300

    Added interface for map options (settings)

--- src/lib/interfaces/google-map.interface.ts
+++ src/lib/interfaces/google-map.interface.ts
@@ -1,7 +1,14 @@
 export interface IMarker {
-    propertyId: number;
+	propertyId: number;
 	key: string;
 	lat: number;
 	lng: number;
+	zoom: number;
+}
+
+export interface IMapOptions {
+	lat: string;
+	lng: string;
+	mapType: string;
 	zoom: number;
 }

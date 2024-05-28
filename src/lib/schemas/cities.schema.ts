2260fa4cb699c12067a685f1a43ae97b5adabb5b
alanloffler <alanmatiasloffler@gmail.com>
2024-05-28 20:06:13 -0300

    Change string for number on state

--- src/lib/schemas/cities.schema.ts
+++ src/lib/schemas/cities.schema.ts
@@ -4,8 +4,8 @@ export const citiesSchema = z.object({
 	city: z.string().min(3, {
 		message: 'El nombre debe poseer al menos 3 caracteres'
 	}),
-	state: z.string().min(3, {
-		message: 'La provincia debe poseer al menos 3 caracteres'
+	state: z.coerce.number().min(1, {
+		message: 'Debes elegir un estado'
 	}),
 	zip: z.string().min(3, {
 		message: 'El código postal debe poseer al menos 3 caracteres'

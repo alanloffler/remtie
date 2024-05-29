512e1604e9125a29afe1cf0bc3ce2d6342c8bf61
alanloffler <alanmatiasloffler@gmail.com>
2024-05-29 10:06:30 -0300

    New config file for general pages

---
+++ src/lib/config/general.config.ts
@@ -0,0 +1,7 @@
+export const GeneralConfig = {
+    loading: 'Cargando',
+    page404: {
+        title: '404 Error - Página no encontrada',
+        message: 'La página que estás buscando no existe',
+    } 
+}

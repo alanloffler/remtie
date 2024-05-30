b344a76a13f1ed0b69beeecfcf938b968fa22976
alanloffler <alanmatiasloffler@gmail.com>
2024-05-29 21:49:25 -0300

    Change validation type for city

--- src/lib/schemas/cities.schema.ts
+++ src/lib/schemas/cities.schema.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
 
 export const citiesSchema = z.object({
-	city: z.string().min(3, {
-		message: 'El nombre debe poseer al menos 3 caracteres'
+	city: z.coerce.number().min(1, {
+		message: 'Debes elegir una ciudad/estado'
 	}),
 	state: z.coerce.number().min(1, {
-		message: 'Debes elegir un estado'
+		message: 'Debes elegir una provincia'
 	}),
 	zip: z.string().min(3, {
 		message: 'El código postal debe poseer al menos 3 caracteres'

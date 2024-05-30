b352d7a30bdb6eb1bb93515b2a7fd63a7d4ecafc
alanloffler <alanmatiasloffler@gmail.com>
2024-05-30 17:34:08 -0300

    Fixed onClick for menu title active

--- src/components/settings/StickyMenu.tsx
+++ src/components/settings/StickyMenu.tsx
@@ -15,25 +15,25 @@ function StickyMenu({ className }: { className?: string }) {
 
 	return (
 		<div className={`relative mx-auto flex items-center rounded-md bg-slate-200/50 ${className}` }>
-			<Link to={`${APP_URL}/config/productos`} className='flex-1 py-2 text-center text-slate-500'>
-				<button onClick={() => handleClick(1)} className={`flex-1 py-2 text-center ${selectedItem === 1 ? 'text-slate-900' : 'text-slate-500'}`}>
+			<Link to={`${APP_URL}/config/productos`} onClick={() => handleClick(1)} className='flex-1 py-2 text-center text-slate-500'>
+				<div className={`flex-1 py-2 text-center ${selectedItem === 1 ? 'text-slate-900' : 'text-slate-500'}`}>
 					{LayoutConfig.sidebar.menu.products}
-				</button>
+				</div>
 			</Link>
-			<Link to={`${APP_URL}/config/mapa`} className='flex-1 py-2 text-center text-slate-500'>
-				<button onClick={() => handleClick(2)} className={`flex-1 py-2 text-center ${selectedItem === 2 ? 'text-slate-900' : 'text-slate-500'}`}>
+			<Link to={`${APP_URL}/config/mapa`} onClick={() => handleClick(2)} className='flex-1 py-2 text-center text-slate-500'>
+				<div className={`flex-1 py-2 text-center ${selectedItem === 2 ? 'text-slate-900' : 'text-slate-500'}`}>
 					{LayoutConfig.sidebar.menu.map}
-				</button>
+				</div>
 			</Link>
-			<Link to={`${APP_URL}/config/tablero`} className='flex-1 py-2 text-center text-slate-500'>
-				<button onClick={() => handleClick(3)} className={`flex-1 py-2 text-center ${selectedItem === 3 ? 'text-slate-900' : 'text-slate-500'}`}>
+			<Link to={`${APP_URL}/config/tablero`} onClick={() => handleClick(3)} className='flex-1 py-2 text-center text-slate-500'>
+				<div className={`flex-1 py-2 text-center ${selectedItem === 3 ? 'text-slate-900' : 'text-slate-500'}`}>
 					{LayoutConfig.sidebar.menu.dashboard}
-				</button>
+				</div>
 			</Link>
-            <Link to={`${APP_URL}/config/usuarios`} className='flex-1 py-2 text-center text-slate-500'>
-				<button onClick={() => handleClick(4)} className={`flex-1 py-2 text-center ${selectedItem === 4 ? 'text-slate-900' : 'text-slate-500'}`}>
+            <Link to={`${APP_URL}/config/usuarios`} onClick={() => handleClick(4)} className='flex-1 py-2 text-center text-slate-500'>
+				<div className={`flex-1 py-2 text-center ${selectedItem === 4 ? 'text-slate-900' : 'text-slate-500'}`}>
 					{LayoutConfig.sidebar.menu.users}
-				</button>
+				</div>
 			</Link>
 			<div className='absolute bottom-0 left-0 h-[2px] bg-blue-500 transition-transform duration-300' style={{ width: '25%', transform: `translateX(calc(${Number(sessionStorage.getItem('sticky-menu')) - 1} * 100%))` }}></div>
 		</div>

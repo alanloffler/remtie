db00066be7172934d6c5500c5eb0cd1fca9c94c9
alanloffler <alanmatiasloffler@gmail.com>
2024-05-31 18:14:41 -0300

    ..

--- src/lib/image-util.ts
+++ src/lib/image-util.ts
@@ -1,9 +1,11 @@
 /* Used for vite.js only
  * Function for images path generated dynamically (like in Carousel)
  */
-function getImageURL(name: string) {
+async function getImageURL(name: string) {
 	// return new URL(`../../../api-nest-mysql/uploads/${name}`, import.meta.url).href;
-	return new URL(`../assets/photos/${name}`, import.meta.url).href;
+	// return new URL(`../assets/photos/${name}`, import.meta.url).href;
+	// return new URL(`/public/assets/photos/${name}`, import.meta.url).href;
+    return (await import('/photos/' + name)).default;
 }
 
 export { getImageURL };
